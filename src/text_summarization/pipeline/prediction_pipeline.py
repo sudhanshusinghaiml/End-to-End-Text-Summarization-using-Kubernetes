b46e19618641789aad8bdf9eafc7178c507c14fd
Sudhanshu Singh <74963600+sudhanshusinghaiml@users.noreply.github.com>
2024-06-12 10:40:24 +0530

    Added logic to donload latest models

--- src/text_summarization/pipeline/prediction_pipeline.py
+++ src/text_summarization/pipeline/prediction_pipeline.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import os
 from src.text_summarization.config.config_manager import ConfigurationManager
 from src.text_summarization.config.aws_storage_operations import S3Operations
@@ -7,12 +8,23 @@ from src.text_summarization.logger import logging
 import boto3
 
 
-
 class PredictionPipeline:
     def __init__(self):
         self.config = ConfigurationManager().get_prediction_pipeline_config()
         self.s3_storage = boto3.client("s3")
 
+    @staticmethod
+    def is_modified_within_last_24_hours(file_path):
+        """This method is used to check if the downloaded files are latest or not"""
+        
+        last_modified_time = os.path.getmtime(file_path)
+        current_time = datetime.now()
+        last_modified_datetime = datetime.fromtimestamp(last_modified_time)
+        logging.info(f" File {file_path} last modified at {last_modified_datetime} and difference between modified and current datetime is {current_time - last_modified_datetime}")
+
+        return (current_time - last_modified_datetime) < timedelta(hours=24)
+
+    
     def get_latest_model_from_s3(self):
         logging.info(f"Downloading latest model from S3 Bucket")
       
@@ -35,7 +47,6 @@ class PredictionPipeline:
                     )
         else:
             logging.info(f"No files found in folder 'model' of bucket {self.config.model_bucket_name}")
-
 
 
     def get_latest_tokenizer_from_s3(self):
@@ -68,18 +79,31 @@ class PredictionPipeline:
 
         logging.info("Inside PredictionPipeline.predict methods")
 
-        self.get_latest_model_from_s3()
+        for file in os.listdir(self.config.saved_model_path):
+            status = self.is_modified_within_last_24_hours(os.path.join(self.config.saved_model_path, file))
+            if status:
+                logging.info("Latest Models are available in the local directory")
+                logging.info("Latest Models will be downloaded after 24 hours")
+            else:
+                logging.info("Dowloading latest Models from S3 Bucket")
+                self.get_latest_model_from_s3()
 
-        self.get_latest_tokenizer_from_s3()
+        for file in os.listdir(self.config.tokenizer_path):
+            status = self.is_modified_within_last_24_hours(os.path.join(self.config.tokenizer_path, file))
+            if status:
+                logging.info("Latest Tokenizer is available in the local directory")
+                logging.info("Latest Tokenizer will be downloaded after 24 hours")
+            else:
+                logging.info("Dowloading latest Tokenizer from S3 Bucket")
+                self.get_latest_tokenizer_from_s3()
+
 
         logging.info(f"Tokenizer Path - {self.config.tokenizer_path}")
-
         tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
         
         gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
 
         logging.info(f"Downloaded Model Path - {self.config.saved_model_path}")
-
         pipe = pipeline("summarization", model = self.config.saved_model_path, tokenizer=tokenizer)
 
         print(f"Dialogue: {text}")
